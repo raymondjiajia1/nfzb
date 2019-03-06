7f1731b7577c3c9f71c17bc6f3ac4655b7a5341c
zq6071339378 <zq6071339378@163.com>
2019-03-06 18:35:21 +0800

    草案发起

--- WebRoot/legislation/assets/util/util.js
+++ WebRoot/legislation/assets/util/util.js
@@ -18,7 +18,6 @@
         return serializeObj;
     };
 })(jQuery);
-
 function onlyNum() {
     if(!(event.keyCode==46)&&!(event.keyCode==8)&&!(event.keyCode==37)&&!(event.keyCode==39))
         if(!((event.keyCode>=48&&event.keyCode<=57)||(event.keyCode>=96&&event.keyCode<=105)))
